99ec79828a6ed7a69aa9e764cda11d6fee3d1701
Happy Igho Umukoro <princeigho74@gmail.com>
2025-11-10 08:07:16 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EduSpark from './components/EduSpark';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EduSpark />
+    </div>
+  );
+}
+
+export default App;
